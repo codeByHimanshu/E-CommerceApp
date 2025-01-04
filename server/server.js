1138ab77f645f115d93ea2b9d5c392f253e82ee1
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-01-04 15:52:03 +0530

    backend update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ dotenv.config();
 const connectDB = require('./config/db');
 connectDB();
 
-app.get()
+app.get( '/', (req, res) => {
+  res.send('Hello World');
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
