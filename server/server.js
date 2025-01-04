15d58a032afd42a3ba3b44f363bf2d3c9f858cce
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-01-04 15:51:19 +0530

    react configration

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 const connectDB = require('./config/db');
 connectDB();
 
+app.get()
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
