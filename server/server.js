00622fe4488e80684d4dd99ff1a0295853f4ecbd
codeByHimanshu <115619706+codeByHimanshu@users.noreply.github.com>
2025-01-06 14:42:32 +0530

    logout route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ app.get( '/', (req, res) => {
   res.send('Hello World');
   const {name ,email, password} = req.body;
 });
+
+app.post('/', (req,res) => {
+res.send('hello world')
+})
+app.post('/', (req,res) => {
+res.send('hello world')
+})
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
